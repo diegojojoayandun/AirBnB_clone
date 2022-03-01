c930bd95f954f5cf4803fd558322ef4d454ab507
diegojojoayandun <3922@holbertonschool.com>
2022-02-28 20:50:17 -0500

    update EOF, quit and hit ENTER+blankline

--- console.py
+++ console.py
@@ -11,6 +11,19 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "  # command line prompt eq to hbnb
     pass
 
+    def do_EOF(self, line):
+        """Ctrl + D command, Exit the program."""
+        print()
+        return True
+
+    def do_quit(self, line):
+        """Quit Command to Exit the program."""
+        return True
+
+    def emptyline(self):
+        """Doesn't do anything on an empty line + ENTER."""
+        pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
