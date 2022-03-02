cb738464c7681b833adaf94f179dcb54940566be
diegojojoayandun <3922@holbertonschool.com>
2022-03-02 08:52:22 -0500

    Fix Reload method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,7 +3,6 @@
 and deserializes JSON file to instances."""
 
 from json import dump, load
-from os import path
 from models.base_model import BaseModel
 
 class FileStorage:
@@ -19,24 +18,24 @@ class FileStorage:
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
-        k = obj.__class__.__name__ + "." + str(obj.id)
-        self.__objects[k] = obj
+        self.__objects[obj.__class__.__name__ + "." + str(obj.id)] = obj
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
         json_objects = {}
         for key in self.__objects:
             json_objects[key] = self.__objects[key].to_dict()
-        with open(FileStorage.__file_path, 'w') as f:
+        with open(self.__file_path, 'w') as f:
             dump(json_objects, f)
 
     def reload(self):
         """deserializes the JSON file to __objects if the JSON file exists
         otherwise, do nothing.
         If the file doesn’t exist, no exception should be raised)"""
-        if (path.isfile(self.__file_path)):
-            with open(self.__file_path, 'r', encoding="utf-8") as fname:
-                l_json = load(fname)
-                for k, v in l_json.items():
-                    self.__objects[k] = eval(
-                        v['__class__'])(**v)
+        try:
+            with open(self.__file_path, 'r', encoding='utf-8') as f:
+                jo = load(f)
+            for k,v in jo.items():
+                self.__objects[k] = eval(v["__class__"])(**v)
+        except:
+            pass
