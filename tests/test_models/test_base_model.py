bba9fc2c9c710d563fbede97f4ea1986e70e779d
cristian <cristaker1927@hotmail.com>
2022-03-03 14:59:52 -0800

    Test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,6 +3,8 @@
 
 import unittest
 from models.base_model import BaseModel
+import datetime
+
 
 class BaseModelTest(unittest.TestCase):
     """Suite of console test"""
@@ -21,5 +23,23 @@ class BaseModelTest(unittest.TestCase):
         self.assertEqual(self.my_model.my_number, my_model_json['my_number'])
         self.assertEqual('BaseModel', my_model_json['__class__'])
 
-    if __name__ == '__main__':
-        unittest.main()
+    def testsaveUpdate(self):
+        """Checks if save method updates the instances update_at"""
+
+        self.my_model.first_name = "Holberton"
+        self.my_model.save()
+
+        self.assertIsInstance(self.my_model.created_at, datetime.datetime)
+        self.assertIsInstance(self.my_model.updated_at, datetime.datetime)
+
+        first_dic = self.my_model.to_dict()
+
+        self.my_model.first_name = "School"
+        self.my_model.save()
+        second_dic = self.my_model.to_dict()
+
+        self.assertEqual(first_dic['created_at'], second_dic['created_at'])
+        self.assertNotEqual(first_dic['updated_at'], second_dic['updated_at'])
+
+if __name__ == '__main__':
+    unittest.main()
