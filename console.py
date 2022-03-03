14e5e7c3af8498c12cc3aeb3c3befdd60812cf2e
cristian <cristaker1927@hotmail.com>
2022-03-03 12:45:26 -0800

    classes

--- console.py
+++ console.py
@@ -6,9 +6,14 @@ import cmd
 import models
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
-classes = {"BaseModel": BaseModel, "user": User}
+classes = {"BaseModel": BaseModel, "user": User, "state": State, "city": City, "amenity": Amenity, "place": Place, "review": Review}
 
 
 class HBNBCommand(cmd.Cmd):
---
+++ models/review.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""review - module, that inherits from BaseModel"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """class review that inherits from BaseModel."""
+
+    place_id = ""
+    user_id = ""
+    text = ""
